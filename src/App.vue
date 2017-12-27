01317f22fd4fcfc424757f3b18b13f77105cb97b
黄金良 <shmaur@outlook.com>
2017-12-28 00:37:49 +0800

    title/nav

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,31 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <div class="header">
+      <div class="leftLogo">
+        <span>shmaur</span>  因为喜欢所有坚持
+      </div>
+      <ul class="nav">
+        <li>不会前端</li>
+        <li>不会设计</li>
+        <li>不会摄影</li>
+        <li>留点留言</li>
+        <li>欢迎交友</li>
+      </ul>
+    </div>
+    <div class="announcement">
+      <p>公告：这是一个很长很长的公告，很有意义的很帅的一个公告</p>
+    </div>
     <router-view/>
   </div>
 </template>
 
 <script>
+import "../static/mains.scss"
 export default {
   name: 'app'
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="scss">
+
 </style>
